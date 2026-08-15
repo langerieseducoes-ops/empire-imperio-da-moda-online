3225aa6c6a2674279648481e1ea594e3716e5c75
langerieseducoes-ops <langerieseducoes@gmail.com>
2026-08-15 12:19:37 -0300

    Create supabase.js

---
+++ assets/js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SEU_PROJETO";
+const SUPABASE_KEY = "COLE_AQUI_A_ANON_PUBLIC_KEY";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
