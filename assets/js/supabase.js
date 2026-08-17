d9725f6f11e8edc0950ad69dd831199014ca6c60
langerieseducoes-ops <langerieseducoes@gmail.com>
2026-08-17 13:33:13 -0300

    Update Supabase key in supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://ldfiwmiwxyubppatlyyh.supabase.co";
-const SUPABASE_KEY = "SUA_CHAVE_PUBLICA";
+const SUPABASE_KEY = "sb_publishable_iNU8uUkD72JlJ8xVrG8MOw_eCppYMIe";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
